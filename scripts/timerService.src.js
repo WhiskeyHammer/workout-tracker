f63ec98e23e8db227438a31abfbcf1ee7a651a04
Jeremy Wiggins <jewiggins@greenshades.com>
2026-01-19 17:49:54 -0500

    feat: update timer alert sound and bump notification channel to v11
    - Replace beep.wav audio file with updated version
    - Increment ALERT_CHANNEL_ID from v8 to v11 to force Android
      to recreate notification channel with new sound settings

--- scripts/timerService.src.js
+++ scripts/timerService.src.js
@@ -11,7 +11,7 @@ let onCompleteCallback = null;
 
 // CONSTANTS - V7 (New Channel to force sound update)
 const ALERT_ID = 99999; 
-const ALERT_CHANNEL_ID = 'workout-timer-alert-v8'; 
+const ALERT_CHANNEL_ID = 'workout-timer-alert-v11'; 
 const ALERT_SOUND = 'beep'; 
 
 async function init() {
